631e96e1b16648f672200df31c3f2fc523fb1f58
ostapmk <ostapmk@gmail.com>
2019-05-29 23:41:13 +0300

    Register fixed

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def main():
     return render_template('main.html')
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     users = mongo.db.users
     login_user = users.find_one({'name': request.form.to_dict()})
@@ -31,27 +31,30 @@ def login():
         hashpass = bcrypt.hashpw(passwd, bcrypt.gensalt())
         if hashpass == login_user['password']:
             session['username'] = request.form.to_dict()['username']
-            return redirect(url_for('index'))
+            return redirect(url_for('/'))
+    else:
+        return 'Invalid username/password combination'
 
-    return 'Invalid username/password combination'
 
-
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find_one({'name': request.form.to_dict()})
-
-        if existing_user is None:
-            passwd = request.form.to_dict()['pass'].encode()
-            hashpass = bcrypt.hashpw(passwd, bcrypt.gensalt())
-            users.insert({'name': request.form.to_dict()['username'], 'password': hashpass})
-            session['username'] = request.form.to_dict()['username']
-            return redirect(url_for('index'))
-
-        return 'That username already exists!'
-
+@app.route('/register', methods=['GET'])
+def register_get():
     return render_template('register.html')
+
+
+@app.route('/register', methods=['POST'])
+def register_post():
+    username = request.form.to_dict()['username']
+    password = request.form.to_dict()['password'].encode()
+    existing_user = mongo.db.users.find_one({'username': username})
+
+    if existing_user is None:
+        hash = bcrypt.hashpw(password, bcrypt.gensalt())
+        mongo.db.users.insert({'name': username, 'password': hash})
+        session['username'] = username
+        # TODO: here should be template for logged-in user
+        return redirect(url_for('main'))
+    else:
+        return 'That username already exists!'
 
 
 if __name__ == '__main__':
