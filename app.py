b64083f0e4db77fe56a093cd3b8070dc5a7811d6
ostapmk <ostapmk@gmail.com>
2019-05-29 23:59:02 +0300

    Fixed login error

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
 import bcrypt
+import os
+
 
 app = Flask(__name__)
+mongo = PyMongo(app)
 
 
-import os
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
-
+app.config['SECRET_KEY'] = os.urandom(32)
 app.config['MONGO_DBNAME'] = 'mongologinexample'
 app.config['MONGO_URI'] = 'mongodb+srv://new-user_31:mongo625@firstcluster0-1vctv.mongodb.net/test?retryWrites=true'
-
-mongo = PyMongo(app)
 
 
 @app.route('/')
@@ -20,19 +18,27 @@ def main():
     return render_template('main.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    users = mongo.db.users
-    login_user = users.find_one({'name': request.form.to_dict()})
+@app.route('/login', methods=['GET'])
+def login_get():
+    return render_template('login.html')
 
-    if login_user:
-        login_user['password'] = bcrypt.gensalt()
-        passwd = request.form.to_dict()['pass'].encode()
-        hashpass = bcrypt.hashpw(passwd, bcrypt.gensalt())
-        if hashpass == login_user['password']:
-            session['username'] = request.form.to_dict()['username']
-            return redirect(url_for('/'))
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.to_dict()['username']
+    password = request.form.to_dict()['password'].encode()
+    login_user = mongo.db.users.find_one({'name': username})
+
+    if login_user is not None:
+        if bcrypt.checkpw(password, login_user['password']):
+            session['username'] = username
+            # TODO: here should be user environment
+            return redirect(url_for('main'))
+        else:
+            # TODO: here should be helpful & beautiful message
+            return "Invalid password"
     else:
+        # TODO: here should be helpful & beautiful message
         return 'Invalid username/password combination'
 
 
